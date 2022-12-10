81ffc5b3351aebc16736526b58d9ed78ce568c7e
xXIldenXx <jdleonor2@up.edu.ph>
2022-12-11 00:22:39 +0800

    Added classify_pollen feature

--- count_pollen.py
+++ count_pollen.py
@@ -27,6 +27,13 @@ def count_pollen(mainImg, outputImgName, cannyEdgeLow, cannyEdgeHigh, houghParam
             return ((s2 - s1) / (r2 - r1)) * (pix - r1) + s1
         else:
             return ((255 - s2) / (255 - r2)) * (pix - r2) + s2
+    
+    def classify_pollen(image, x, y, r, threshold):
+        circle_img = np.zeros((image.shape[0], image.shape[1]), np.uint8)
+        cv2.circle(circle_img, (x, y,), r, (255, 255, 255), -1)
+        if cv2.mean(image, mask=circle_img)[0] > threshold: 
+            return 1
+        return 0
 
     # Define parameters.
     r1 = 100
@@ -55,14 +62,13 @@ def count_pollen(mainImg, outputImgName, cannyEdgeLow, cannyEdgeHigh, houghParam
         dilation, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     # Initialize the list
-    Cell_count, x_count, y_count = [], [], []
+    pollen_count, x_count, y_count, pollen_class = [], [], [], []
 
     # read original image, to display the circle and center detection 
     display = cv2.imread(mainImg)
 
     # hough transform with modified circular parameters
-    circles = cv2.HoughCircles(th4, cv2.HOUGH_GRADIENT, 1.75, minDist,
-                               param1=cannyEdgeHigh, param2=houghParam2, minRadius=minRad, maxRadius=maxRad)
+    circles = cv2.HoughCircles(th4, cv2.HOUGH_GRADIENT, 1.75, minDist, param1=cannyEdgeHigh, param2=houghParam2, minRadius=minRad, maxRadius=maxRad)
 
     # circle detection and labeling using hough transformation
     if circles is not None:
@@ -70,23 +76,27 @@ def count_pollen(mainImg, outputImgName, cannyEdgeLow, cannyEdgeHigh, houghParam
         circles = np.round(circles[0, :]).astype("int")
         # loop over the (x, y) coordinates and radius of the circles
         for (x, y, r) in circles:
-            cv2.circle(display, (x, y), r, (0, 255, 0), 2)
-            cv2.rectangle(display, (x - 2, y - 2),
-                          (x + 2, y + 2), (0, 128, 255), -1)
-            Cell_count.append(r)
+            pollen_class.append(classify_pollen(claheNorm, x, y, r, 70))
+            if pollen_class[-1]:
+                cv2.circle(display, (x, y), r, (255, 255, 0), 2)
+                cv2.rectangle(display, (x - 2, y - 2), (x + 2, y + 2), (150, 128, 255), -1)
+            else:
+                cv2.circle(display, (x, y), r, (0, 255, 0), 2)
+                cv2.rectangle(display, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)
+            pollen_count.append(r)
             x_count.append(x)
             y_count.append(y)
         # show the output image
         cv2.imwrite(outputImgName, display)
 
     # display the count of pollen
-    print(len(Cell_count))
+    # print(len(pollen_count))
     # Total number of radius
-    print(Cell_count)
+    # print(pollen_count)
     # X co-ordinate of circle
-    print(x_count)
+    # print(x_count)
     # Y co-ordinate of circle
-    print(y_count)
+    # print(y_count)
 
     # image write statements:
     # uncomment if you want to see step-by-step process
@@ -97,7 +107,7 @@ def count_pollen(mainImg, outputImgName, cannyEdgeLow, cannyEdgeHigh, houghParam
     cv2.imwrite("th4.png", th4)
 
 
-mainImg = "practice_image_1.jpg"
+mainImg = "practice_image_2.jpg"
 outputImgName = "main_output.png"
 
 # params to be modified to detect pollen
@@ -109,5 +119,4 @@ minDist = 70    # min distance between circles
 minRad = 10     # min radius of circles
 maxRad = 60     # max radius of circles
 
-count_pollen(mainImg, outputImgName, cannyEdgeLow,
-             cannyEdgeHigh, houghParam2, minDist, minRad, maxRad)
+count_pollen(mainImg, outputImgName, cannyEdgeLow, cannyEdgeHigh, houghParam2, minDist, minRad, maxRad)
